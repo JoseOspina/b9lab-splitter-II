ef5d6b647effd25b8b57f64ad315d6ea8f241b9c
Jose Ospina <pepo.ospina@gmail.com>
2017-09-11 19:40:58 +0200

    Refactoring with guidelins from Rob

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,107 +2,49 @@ pragma solidity ^0.4.4;
 
 contract Splitter {
 
-	address public sender;
+	mapping (address => uint) public balances;
 
-	address public receiver1;
-	uint 		public receiver1Balance;
-
-	address public receiver2;
-	uint 		public receiver2Balance;
-
-	event   LogReceiver1Set(address receiverAddress);
-	event   LogReceiver2Set(address receiverAddress);
-	event   LogFundsFromSenderReceived(uint amount);
-	event   LogReceiver1Withdrawed(uint amount);
-	event   LogReceiver2Withdrawed(uint amount);
-
+	event   LogFundsReceived(uint amount, address sender, address receiver1, address receiver2);
+	event   LogFundsWithdrawn(uint amount, address receiver);
 
 	function Splitter() {
-		/* the person sending funds to be splitted
-			will be the contract creator */
-		sender = msg.sender;
 	}
-
-
-	/* receivers are set with setters and not in the
-	constructor */
-	function setReceiver1(address _address) {
-		if (msg.sender != sender) throw;
-		receiver1 = _address;
-		LogReceiver1Set(receiver1);
-	}
-
-	function setReceiver2(address _address) {
-		if (msg.sender != sender) throw;
-		receiver2 = _address;
-		LogReceiver2Set(receiver2);
-	}
-
-	/* no reason to implement a balance when there is already
-	a function that gets the balance of a contract address */
-
-	/*function getBalance() public returns(uint) {
-		return this.balance;
-	}*/
 
 	/* Fallback function to check if ether comes from sender*/
-	function () payable {
-		if (msg.sender == sender) {
-			receiver1Balance += msg.value / 2;
-			receiver2Balance += msg.value / 2;
-			LogFundsFromSenderReceived(msg.value);
+	function split(address receiver1, address receiver2) payable returns (bool success) {
+		balances[receiver1] += msg.value / 2;
+		balances[receiver2] += msg.value / 2;
+
+		/* handle odd numbers */
+		uint remainder = msg.value - (msg.value / 2) - (msg.value / 2);
+		if (remainder > 0) {
+			if(!msg.sender.send(remainder)) {
+				// problem sending remainder back to sender
+			}
 		}
+
+		LogFundsReceived(msg.value, msg.sender, receiver1 , receiver2);
+		return true;
 	}
 
-	function getSenderBalance() public returns (uint) {
-		return sender.balance;
+	function getBalanceOf(address receiver) public returns (uint) {
+		return balances[receiver];
 	}
 
-	function getReceiver1Balance() public returns (uint) {
-		return receiver1.balance;
-	}
-
-	function getReceiver2Balance() public returns (uint) {
-		return receiver2.balance;
-	}
-
-	/* pattern in which receiver 1 and 2 can withdraw their funds
-		instead of sending them every time funds arrive */
 	function getMyFunds() public returns (bool success){
-		if (msg.sender != receiver1 && msg.sender != receiver2) throw;
+		if (balances[msg.sender] == 0) throw;
 
-		uint fundsToSend;
-
-		if (msg.sender == receiver1) {
-			fundsToSend = receiver1Balance;
-			/* reset funds before sending */
-			receiver1Balance = 0;
-			if(!receiver1.send(fundsToSend)) {
-				/* sent failed, so keep the funds available */
-				receiver1Balance = fundsToSend;
-			}
-			LogReceiver1Withdrawed(fundsToSend);
-			return true;
+		uint orgBalance = balances[msg.sender];
+		/* reset funds before sending */
+		balances[msg.sender] = 0;
+		if(!msg.sender.send(orgBalance)) {
+			/* sent failed, so keep the funds available */
+			balances[msg.sender] = orgBalance;
+			return false;
 		}
 
-		if (msg.sender == receiver2) {
-			fundsToSend = receiver2Balance;
-			/* reset funds before sending */
-			receiver2Balance = 0;
-			if(!receiver2.send(fundsToSend)) {
-				/* sent failed, so keep the funds available */
-				receiver2Balance = fundsToSend;
-			}
-			LogReceiver2Withdrawed(fundsToSend);
-			return true;
-		}
-
-		return false;
+		LogFundsWithdrawn(orgBalance, msg.sender);
+		return true;
 	}
 
-	function kill() public {
-		if(msg.sender == sender) {
-			selfdestruct(sender);
-		}
-	}
 }
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -16,23 +16,11 @@
           <td>{{ splitterAddressBalance.toFixed(2) }}</td>
           <td>-</td>
         </tr>
-        <tr>
-          <td>Sender</td>
-          <td>{{ splitterSender.substring(2, 10) }}</td>
-          <td>{{ senderBalance.toFixed(2) }}</td>
-          <td>-</td>
-        </tr>
-        <tr>
-          <td>Receiver 1 </td>
-          <td>{{ receiver1Address.substring(2, 10) }}</td>
-          <td>{{ receiver1Balance.toFixed(2) }}</td>
-          <td>{{ receiver1BalanceInSplitter.toFixed(2) }}</td>
-        </tr>
-        <tr>
-          <td>Receiver 2</td>
-          <td>{{ receiver2Address.substring(2, 10) }}</td>
-          <td>{{ receiver2Balance.toFixed(2) }}</td>
-          <td>{{ receiver2BalanceInSplitter.toFixed(2) }}</td>
+        <tr v-for="account in accounts">
+          <td>User</td>
+          <td>{{ account.address.substring(2, 10) }}</td>
+          <td>{{ account.balance.toFixed(2) }}</td>
+          <td>{{ account.balanceInSplitter.toFixed(2) }}</td>
         </tr>
       </table>
     </div>
@@ -43,36 +31,25 @@
       <label class="w3-left"><b>from:</b></label>
       <select class="w3-input" v-model="from">
         <option v-for="account in accounts">
-          {{ account }}
+          {{ account.address }}
         </option>
       </select>
     </div>
-    <div v-if="from === splitterSender" class="">
+    <div class="w3-row">
       <h3>You are the Sender of the splitter!</h3>
-      <div class="w3-row-padding w3-margin-top">
-        <div class="w3-col m8">
-          <input class="w3-input" v-model="receiver1ToSet">
-        </div>
-        <div class="w3-col m4 ">
-          <button @click="setReceiver1" class="w3-button w3-blue w3-round-large">Set Receiver 1</button>
-        </div>
+      <div class="w3-margin-top">
+        <select class="w3-input" v-model="receiver1">
+          <option v-for="account in accounts">
+            {{ account.address }}
+          </option>
+        </select>
       </div>
-      <div class="w3-row-padding w3-margin-top">
-        <div class="w3-col m8">
-          <input class="w3-input" v-model="receiver2ToSet">
-        </div>
-        <div class="w3-col m4 ">
-          <button @click="setReceiver2" class="w3-button w3-blue w3-round-large">Set Receiver 2</button>
-        </div>
-      </div>
-    </div>
-
-    <div v-if="isReceiver1 || isReceiver2" class="">
-      <h3>You are one of the receivers of the splitter, and you have {{ receiverBalanceInSplitter }} ether available!</h3>
-      <div class="w3-row-padding w3-margin-top">
-        <div class="w3-col s12 ">
-          <button @click="withDrawReceiverFunds()" class="w3-button w3-blue w3-round-large">Get my funds!</button>
-        </div>
+      <div class="w3-margin-top">
+        <select class="w3-input" v-model="receiver2">
+          <option v-for="account in accounts">
+            {{ account.address }}
+          </option>
+        </select>
       </div>
     </div>
 
@@ -81,8 +58,12 @@
         <input class="w3-input" type="number" v-model="valueToSend">
       </div>
       <div class="w3-col m4 ">
-        <button @click="sendFunds()" class="w3-button w3-blue w3-round-large">Send To Splitter</button>
+        <button @click="sendFunds()" class="w3-button w3-blue w3-round-large">Send funds</button>
       </div>
+    </div>
+
+    <div class="w3-row">
+      <button @click="withdrawFunds()" class="w3-button w3-blue w3-round-large">Get my funds!</button>
     </div>
 
     <br>
@@ -90,12 +71,10 @@
     <div class="w3-row">
       <h4 v-if="events.length === 0">(empty)</h4>
       <ul class="w3-ul w3-card-2">
-        <li v-for="event in lastEvents">{{ event }}</li>
+        <li v-for="event in events">{{ event }}</li>
       </ul>
     </div>
 
-
-    <br>
   </div>
 </template>
 
@@ -107,153 +86,70 @@ export default {
   data () {
     return {
       splitterInstance: null,
-      splitterSender: '',
       accounts: [],
       from: '',
-      receiver1ToSet: '',
-      receiver2ToSet: '',
-      receiver1Address: '',
-      receiver2Address: '',
-      splitterBalance: 0,
+      receiver1: '',
+      receiver2: '',
       splitterAddressBalance: 0,
-      senderBalance: 0,
-      receiver1Balance: 0,
-      receiver2Balance: 0,
       valueToSend: 0,
-      receiver1BalanceInSplitter: 0,
-      receiver2BalanceInSplitter: 0,
       events: []
     }
   },
 
   computed: {
-    isReceiver1 () {
-      return this.from === this.receiver1Address
-    },
-    isReceiver2 () {
-      return this.from === this.receiver2Address
-    },
-    receiverBalanceInSplitter () {
-      if (this.isReceiver1) {
-        return this.receiver1BalanceInSplitter
-      }
-
-      if (this.isReceiver2) {
-        return this.receiver2BalanceInSplitter
-      }
-
-      return 0
-    },
-    lastEvents () {
-      return this.events
-      // events are not well ordered always. Spcific sorting logic needed
-      // if (this.events.length > 5) {
-      //   return this.events.slice(this.events.length - 5 - 1, this.events.length - 1)
-      // } else {
-      //   return this.events
-      // }
-    }
   },
 
   methods: {
-    setReceiver1 () {
-      var ok = true
-      if (this.receiver1ToSet === '') {
-        ok = false
-      }
-      if (ok) {
-        this.splitterInstance.setReceiver1(this.receiver1ToSet, { from: this.from }).then((res) => {
-          return this.updateReceiver1()
-        })
-      }
-    },
-    setReceiver2 () {
-      var ok = true
-      if (this.receiver2ToSet === '') {
-        ok = false
-      }
-      if (ok) {
-        this.splitterInstance.setReceiver2(this.receiver2ToSet, { from: this.from }).then((res) => {
-          return this.updateReceiver2()
-        })
-      }
-    },
+
     updateAll () {
-      this.updateReceiver1()
-      this.updateReceiver2()
-      this.updateAllBalances()
-    },
-    updateReceiver1 () {
-      this.splitterInstance.receiver1().then((receiver1) => {
-        this.receiver1Address = receiver1
-        this.updateBalanceOf(this.receiver1Address)
-      })
-
-      this.splitterInstance.receiver1Balance().then((balance) => {
-        this.receiver1BalanceInSplitter = Number(web3.fromWei(balance))
-      })
-    },
-    updateReceiver2 () {
-      this.splitterInstance.receiver2().then((receiver2) => {
-        this.receiver2Address = receiver2
-        this.updateBalanceOf(this.receiver2Address)
-      })
-
-      this.splitterInstance.receiver2Balance().then((balance) => {
-        this.receiver2BalanceInSplitter = Number(web3.fromWei(balance))
-      })
-    },
-    updateAllBalances () {
-      this.updateBalanceOf(this.splitterInstance.address)
-      this.updateBalanceOf(this.splitterSender)
-      this.updateBalanceOf(this.receiver1Address)
-      this.updateBalanceOf(this.receiver2Address)
-    },
-    updateBalanceOf (address) {
-      if (address === '0x0000000000000000000000000000000000000000') {
-        return
-      }
-      if (address === '') {
-        return
-      }
-      web3.eth.getBalance(address, (err, balance) => {
-        if (err) {
-          console.log(err)
-        } else {
-          var balanceInEth = Number(web3.fromWei(balance))
-          switch (address) {
-            case this.splitterInstance.address:
-              this.splitterAddressBalance = balanceInEth
-              break
-
-            case this.splitterSender:
-              this.senderBalance = balanceInEth
-              break
-
-            case this.receiver1Address:
-              this.receiver1Balance = balanceInEth
-              break
-
-            case this.receiver2Address:
-              this.receiver2Balance = balanceInEth
-              break
-          }
+      web3.eth.getAccountsPromise().then((accounts) => {
+        this.accounts = []
+        for (var ix in accounts) {
+          this.accounts.push({
+            address: accounts[ix],
+            balance: 0,
+            balanceInSplitter: 0
+          })
         }
+
+        /* correct way to loop with promises */
+        const getBalances = (ix) => {
+          web3.eth.getBalancePromise(this.accounts[ix].address).then((balance) => {
+            console.log('balance received: ' + ix)
+            console.log(this.splitterInstance)
+            console.log(ix)
+            console.log('length:' + this.accounts.length)
+
+            this.accounts[ix].balance = Number(web3.fromWei(balance))
+
+            // if (ix < this.accounts.length - 1) {
+            //   getBalances(ix + 1)
+            // }
+            console.log('address:' + this.accounts[ix].address)
+            return this.splitterInstance.getBalanceOf(this.accounts[ix].address, { from: this.from })
+          }).then((balance) => {
+            console.log('splitter balance received: ' + ix)
+            console.log(balance)
+            this.setAccountSplitterBalance(ix, balance)
+          })
+        }
+
+        getBalances(0)
       })
+    },
+    setAccountBalance (ix, balance) {
+
+    },
+    setAccountSplitterBalance (ix, balance) {
+      this.accounts[ix].balanceInSplitter = Number(web3.fromWei(balance))
     },
     sendFunds () {
-      web3.eth.sendTransaction({
-        from: this.from,
-        to: this.splitterInstance.address,
-        value: web3.toWei(this.valueToSend, 'ether')
-      }, (err, txn) => {
-        if (err) {
-          console.log(err)
-        }
+      this.splitterInstance.split(this.receiver1, this.receiver2, { from: this.from, value: web3.toWei(this.valueToSend, 'ether') })
+      .then((success) => {
         this.updateAll()
       })
     },
-    withDrawReceiverFunds () {
+    withdrawFunds () {
       this.splitterInstance.getMyFunds({ from: this.from }).then(() => {
         this.updateAll()
       })
@@ -263,44 +159,24 @@ export default {
   mounted () {
     Splitter.deployed().then((instance) => {
       this.splitterInstance = instance
-      return this.splitterInstance.sender()
-    })
-    .then((sender) => {
-      this.splitterSender = sender
       this.updateAll()
 
       /* check events */
-      this.splitterInstance.LogFundsFromSenderReceived({}, { fromBlock: 0 })
+      this.splitterInstance.LogFundsReceived({}, { fromBlock: 0 })
       .watch((err, event) => {
         if (err) {
           console.log(err)
         }
-        this.events.push(Number(web3.fromWei(event.args.amount)) + ' ether sent by the Sender at block ' + event.blockNumber)
+        this.events.push(Number(web3.fromWei(event.args.amount)) + ' ether sent by ' + event.args.sender + ' to ' + event.args.receiver1 + ' and ' + event.args.receiver2)
       })
 
-      this.splitterInstance.LogReceiver1Withdrawed({}, { fromBlock: 0 })
+      this.splitterInstance.LogFundsWithdrawn({}, { fromBlock: 0 })
       .watch((err, event) => {
         if (err) {
           console.log(err)
         }
-        this.events.push(Number(web3.fromWei(event.args.amount)) + ' ether withdrawn by the Rceiver 1 at block ' + event.blockNumber)
+        this.events.push(Number(web3.fromWei(event.args.amount)) + ' ether withdrawn by ' + event.args.receiver)
       })
-
-      this.splitterInstance.LogReceiver2Withdrawed({}, { fromBlock: 0 })
-      .watch((err, event) => {
-        if (err) {
-          console.log(err)
-        }
-        this.events.push(Number(web3.fromWei(event.args.amount)) + ' ether withdrawn by the Receiver 2 at block ' + event.blockNumber)
-      })
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-
-    web3.eth.getAccountsPromise().then((accounts) => {
-      this.accounts = accounts
-      this.from = accounts[0]
     })
   }
 }
